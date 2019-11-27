04e0b7c30b95839c7043ea68b7bfc684ee8ff457
Ryan Fleck <ryan.fleck@protonmail.com>
2019-11-26 22:25:31 -0500

    Add Q&A models.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Influx
 Learning Management System in Django
+
+- Django [tutorial](https://docs.djangoproject.com/en/2.2/intro/tutorial02/)
+- Heroku [dashboard](https://dashboard.heroku.com/apps/influx-lms)
--- influx/settings.py
+++ influx/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import django_heroku
+import dj_database_url
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -32,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,8 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -74,12 +78,8 @@ WSGI_APPLICATION = 'influx.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST' # Was UTC
 
 USE_I18N = True
 
@@ -124,3 +124,4 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
+del DATABASES['default']['OPTIONS']['sslmode']
