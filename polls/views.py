cb3c04872a8ed2150f852d5daa1d75b04b98e0d8
Ryan Fleck <ryan.fleck@protonmail.com>
2019-11-27 09:20:33 -0500

    Extend templates from a base template.

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 
 from .models import Question
@@ -18,6 +18,7 @@ def index(request):
 def detail(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
+        # alternatively, get_object_or_404(Question, pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exist!")
 
@@ -25,8 +26,10 @@ def detail(request, question_id):
 
 
 def vote(request, question_id):
+    q = get_object_or_404(Question, pk=question_id)
     return HttpResponse("You are voting on question %s." % question_id)
 
 
 def results(request, question_id):
+    q = get_object_or_404(Question, pk=question_id)
     return HttpResponse("Results for question %s." % question_id)
