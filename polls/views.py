912b3fed46a7457a098b529b8fbd7db98bb91182
Ryan Fleck <ryan.fleck@protonmail.com>
2019-11-27 12:14:39 -0500

    Write results page.

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
 
@@ -27,9 +28,25 @@ def detail(request, question_id):
 
 def vote(request, question_id):
     q = get_object_or_404(Question, pk=question_id)
-    return HttpResponse("You are voting on question %s." % question_id)
+
+    try:
+        # Get choice from POST request!
+        c = q.choice_set.get(pk=request.POST['choice'])
+    
+    except (KeyError, Choice.DoesNotExist):
+
+        return render(request, 'detail.html', {
+            'question': q,
+            'error_message': "Please make a selection."
+        })
+    
+    else:
+        c.votes += 1
+        c.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(q.id,)))
+
 
 
 def results(request, question_id):
     q = get_object_or_404(Question, pk=question_id)
-    return HttpResponse("Results for question %s." % question_id)
+    return render(request, 'results.html', {'question':q})
