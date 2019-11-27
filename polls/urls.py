fa48bc765854e0cf27500ae31cfaebcae5037312
Ryan Fleck <ryan.fleck@protonmail.com>
2019-11-27 12:29:30 -0500

    Migrate to generic views.

--- polls/urls.py
+++ polls/urls.py
@@ -7,10 +7,10 @@ app_name = 'polls'
 urlpatterns = [
 
     # Change this to 'poll' after experiment is finished.
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
 
     # A set of pages for looking at poll questions
-    path('<int:question_id>', views.detail, name='detail'),
-    path('<int:question_id>/results', views.results, name='results'),
-    path('<int:question_id>/vote', views.vote, name='vote'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,29 +1,29 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
 # Create your views here.
 
 
-def index(request):
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'questions'
 
-    questions = Question.objects.order_by('-pub_date')[:5]
-    #output = ', '.join([q.question_text for q in questions])
-    context = {'questions': questions}
-
-    return render(request, 'index.html', context)
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-        # alternatively, get_object_or_404(Question, pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist!")
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-    return render(request, 'detail.html', {'question': question})
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
@@ -32,21 +32,15 @@ def vote(request, question_id):
     try:
         # Get choice from POST request!
         c = q.choice_set.get(pk=request.POST['choice'])
-    
+
     except (KeyError, Choice.DoesNotExist):
 
-        return render(request, 'detail.html', {
+        return render(request, 'polls/detail.html', {
             'question': q,
             'error_message': "Please make a selection."
         })
-    
+
     else:
         c.votes += 1
         c.save()
         return HttpResponseRedirect(reverse('polls:results', args=(q.id,)))
-
-
-
-def results(request, question_id):
-    q = get_object_or_404(Question, pk=question_id)
-    return render(request, 'results.html', {'question':q})
